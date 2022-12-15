a6cc7e2a8e95d4d3df5c163c063b1b28c1a82778
Gui <guilherme.g.braz@outlook.com>
2022-12-15 10:37:04 -0300

    Medico DTO atualização

--- Web/src/main/java/med/voll/api/models/dto/MedicoDTO.java
+++ Web/src/main/java/med/voll/api/models/dto/MedicoDTO.java
@@ -1,6 +1,17 @@
 package med.voll.api.models.dto;
 
-import dominio.medico.Especialidade;
+import entities.Email;
+import entities.medico.Especialidade;
+import entities.medico.Medico;
 
-public record MedicoDTO(String nome, String email, String CRM, Especialidade especialidade, EnderecoDTO endereco) {
+public record MedicoDTO(String nome, String email, String CRM,
+                        Especialidade especialidade, EnderecoDTO endereco) {
+    public Medico toMedico() {
+        return new
+                Medico(this.nome,
+                new Email(this.email),
+                this.CRM,
+                this.especialidade,
+                endereco.toEndereco());
+    }
 }
