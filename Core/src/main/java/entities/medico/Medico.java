10d1edd02714f59935de263e7635eaeec5ac440f
Gui <guilherme.g.braz@outlook.com>
2022-12-15 10:36:43 -0300

    Entidade Medico

---
+++ Core/src/main/java/entities/medico/Medico.java
@@ -0,0 +1,59 @@
+package entities.medico;
+
+import entities.Email;
+import entities.Endereco;
+
+public class Medico {
+
+    private String nome;
+    private Email email;
+    private  String CRM;
+    private Especialidade especialidade;
+    private Endereco endereco;
+
+
+    public Medico(String nome, Email email, String CRM, Especialidade especialidade, Endereco endereco) {
+        this.nome = nome;
+        this.email = email;
+        this.CRM = CRM;
+        this.especialidade = especialidade;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    protected void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    protected void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public String getCRM() {
+        return CRM;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    protected void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    protected void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+}
