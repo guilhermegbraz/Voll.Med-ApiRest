06e2f017ca3b2e665a5b59809f5c2bad820f98d7
Gui <guilherme.g.braz@outlook.com>
2022-12-16 11:20:04 -0300

    Refactor Aplication config

--- Web/src/main/java/med/voll/api/ApiApplication.java
+++ Web/src/main/java/med/voll/api/ApiApplication.java
@@ -1,12 +1,15 @@
 package med.voll.api;
 
+import med.voll.configuration.JpaConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@Import(JpaConfiguration.class)
 public class ApiApplication {
 
 	public static void main(String[] args) {
